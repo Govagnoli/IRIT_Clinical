81c8fb7dddc9a9762fd1d8df090cfe488bb5b4c6
Anass Hmadouch <102656228+AnassHmadouch@users.noreply.github.com>
2023-03-21 10:40:06 +0100

    Add files via upload

--- appDash/apps.py
+++ appDash/apps.py
@@ -1,6 +1,12 @@
+import django
 from django.apps import AppConfig
 
 
 class AppdashConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appDash'
+    INSTALLED_APPS = [
+
+        django.contrib.staticfiles
+    ]
+    STATIC_URL = 'plot/'
